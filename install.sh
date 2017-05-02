f093453d6804158ceba71da249334e491af63ea1
guillaumemolter <gmolter@hsph.harvard.edu>
2017-05-02 14:29:20 -0400

    Preventing orphaned containers using --rm

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose run wpcli core install --url="localhost:8080" --title="WordPress" --admin_user="admin" --admin_password="admin" --admin_email="admin@email.com"
-docker-compose run wpcli core multisite-convert
-docker-compose run wpcli plugin activate multisite-taxonomies
+docker-compose run --rm wpcli core install --url="localhost:8080" --title="WordPress" --admin_user="admin" --admin_password="admin" --admin_email="admin@email.com"
+docker-compose run --rm wpcli core multisite-convert
+docker-compose run --rm wpcli plugin activate multisite-taxonomies
 composer install
